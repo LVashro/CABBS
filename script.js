0ea7c75c781a5e9c85fdb50c2731d13158d5d7f9
DoggyID <c2070103@planet.kanazawa-it.ac.jp>
2023-02-19 05:05:12 +1300

    kakikomed filtering feature
    Not sure if this code will work on this environment. Untested.

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let ngwords = [];
 let Id = [];
 let delayInMilliseconds = 1000;
 
+//Load JSON file for the "word ban" feature. Takes about 2000ms async.
+let requestURL = 'https://raw.githubusercontent.com/dariusk/wordfilter/master/lib/badwords.json';
+let request = new XMLHttpRequest(); 
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+const ng = request.onload = function(){ //When the JSON has been loaded.
+    const ngwords = request.response; //ngwords = JS object(array) that contains a list of bad words in it.
+    return{ //Output ngwords(array) outside of the function to utilize it as a list.
+        ngwords 
+    };
+};
+
 // peerオブジェクト
 const peer = new Peer({
     key: '1b703875-3805-4c38-93d2-3c9f2f5c9c57',// 自分のAPIキーを入力
@@ -34,6 +47,13 @@ let count = 0;
 
 // チャットを受信
     room.on('data', function(data){
+        for(i = 0; i < ng().ngwords.length; i++){ //Search for the bad word
+            if(data.data.indexOf(ng().ngwords[i]) != -1){
+                chatlog('Stop that trashtalk');
+                data.data = "Hashtag"; //Convert the bad word with "Hashtag" if the received message contains a bad word.
+                break;
+            }
+        }
         chatlog('ID: ' + data.src + '> ' + data.data); //data.src = 送信者のpeerid, data.data = 送信されたメッセージ
     });
     
